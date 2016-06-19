a41a2383bcfa0fe39a7d8da13715df9b0ddfc528
Christian Köberl <christian.koeberl@porscheinformatik.at>
2016-06-19 13:48:35 +0200

    Better parsing and error handling in package.json handling

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/npm/PackageJsonDependencyScanner.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/npm/PackageJsonDependencyScanner.java
@@ -1,29 +1,30 @@
 package at.porscheinformatik.sonarqube.licensecheck.npm;
 
+import at.porscheinformatik.sonarqube.licensecheck.Dependency;
+import at.porscheinformatik.sonarqube.licensecheck.interfaces.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParser;
-
-import at.porscheinformatik.sonarqube.licensecheck.Dependency;
-import at.porscheinformatik.sonarqube.licensecheck.interfaces.Scanner;
-
 public class PackageJsonDependencyScanner implements Scanner
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PackageJsonDependencyScanner.class);
+
     @Override
     public List<Dependency> scan(File file, String mavenProjectDependencies)
     {
         File packageJsonFile = new File(file, "package.json");
-        final List<Dependency> dependencies = new ArrayList<>();
 
         if (packageJsonFile.exists())
         {
@@ -33,65 +34,78 @@ public class PackageJsonDependencyScanner implements Scanner
                 fis = new FileInputStream(packageJsonFile);
                 JsonReader jsonReader = Json.createReader(fis);
                 JsonObject jsonObject = jsonReader.readObject();
-                JsonObject jsonObjectdependencies = jsonObject.getJsonObject("dependencies");
-                String jsonObjectString = jsonObjectdependencies.toString();
-
-                dependencyParser(dependencies, jsonObjectString, packageJsonFile);
-            }
-            catch (FileNotFoundException e1)
-            {
-                throw new RuntimeException(e1);
-            }
-            return dependencies;
-        }
-        return Collections.emptyList();
-    }
-
-    private void dependencyParser(List<Dependency> dependencies, String jsonObjectString, File packageJsonFile)
-    {
-        JsonParser parser = Json.createParser(new StringReader(jsonObjectString));
-        File nodeModulesFolder = new File(packageJsonFile.getParentFile(), "node_modules");
-
-        while (parser.hasNext())
-        {
-            JsonParser.Event event = parser.next();
-            switch (event)
-            {
-                case KEY_NAME:
-                    if (nodeModulesFolder.exists() && nodeModulesFolder.isDirectory())
-                    {
-                        moduleCheck(nodeModulesFolder, parser.getString(), dependencies);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private void moduleCheck(File nodeModulesFolder, String identifier, List<Dependency> dependencies)
-    {
-        File module = new File(nodeModulesFolder, identifier);
-        checkPackageJsonForDependencies(module, dependencies, identifier);
-    }
-
-    private static void checkPackageJsonForDependencies(File module, List<Dependency> dependencies, String name)
-    {
-        if (module.exists() && module.isDirectory())
-        {
-            File packageFile = new File(module, "package.json");
-
-            try
-            {
-                InputStream fis = new FileInputStream(packageFile);
-                JsonReader jsonReader = Json.createReader(fis);
-                JsonObject jsonObject = jsonReader.readObject();
-                dependencies
-                    .add(new Dependency(name, jsonObject.getString("version"), jsonObject.getString("license")));
+                JsonObject jsonObjectDependencies = jsonObject.getJsonObject("dependencies");
+                if (jsonObjectDependencies != null)
+                {
+                    return dependencyParser(jsonObjectDependencies, packageJsonFile);
+                }
             }
             catch (FileNotFoundException e)
             {
                 throw new RuntimeException(e);
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    private List<Dependency> dependencyParser(JsonObject jsonDependencies, File packageJsonFile)
+    {
+        List<Dependency> dependencies = new ArrayList<>();
+
+        File nodeModulesFolder = new File(packageJsonFile.getParentFile(), "node_modules");
+        if (nodeModulesFolder.exists() && nodeModulesFolder.isDirectory())
+        {
+            for (String key : jsonDependencies.keySet())
+            {
+                moduleCheck(nodeModulesFolder, key, dependencies);
+            }
+        }
+
+        return dependencies;
+    }
+
+    private static void moduleCheck(File nodeModulesFolder, String identifier, List<Dependency> dependencies)
+    {
+        File moduleFolder = new File(nodeModulesFolder, identifier);
+
+        if (moduleFolder.exists() && moduleFolder.isDirectory())
+        {
+            File packageFile = new File(moduleFolder, "package.json");
+
+            try (InputStream fis = new FileInputStream(packageFile))
+            {
+                JsonReader jsonReader = Json.createReader(fis);
+                JsonObject jsonObject = jsonReader.readObject();
+                if (jsonObject != null)
+                {
+                    String license = null;
+                    if (jsonObject.containsKey("license"))
+                    {
+                        license = jsonObject.getString("license");
+                    }
+                    else if(jsonObject.containsKey("licenses"))
+                    {
+                        JsonArray licenses = jsonObject.getJsonArray("licenses");
+                        if (licenses.size() > 0)
+                        {
+                            license = licenses.getJsonObject(0).getString("type");
+                        }
+                    }
+
+                    if (license!= null)
+                    {
+                        dependencies.add(
+                            new Dependency(identifier, jsonObject.getString("version"), license));
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                LOGGER.error("Could not find package.json", e);
+            }
+            catch (Exception e)
+            {
+                LOGGER.error("Error adding dependency " + identifier, e);
             }
         }
     }
