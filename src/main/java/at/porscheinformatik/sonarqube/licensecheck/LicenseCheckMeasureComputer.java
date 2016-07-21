6c6837095d890f03d3c48b49abf1c7b636c923b2
Christian Köberl <christian.koeberl@porscheinformatik.at>
2016-07-21 11:07:37 +0200

    Fix comining of licenses and dependencies

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckMeasureComputer.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckMeasureComputer.java
@@ -6,6 +6,8 @@ import static at.porscheinformatik.sonarqube.licensecheck.LicenseCheckMetrics.IN
 import static at.porscheinformatik.sonarqube.licensecheck.LicenseCheckMetrics.LICENSE;
 import static at.porscheinformatik.sonarqube.licensecheck.LicenseCheckMetrics.MAVENLICENSE;
 import static at.porscheinformatik.sonarqube.licensecheck.LicenseCheckPropertyKeys.ACTIVATION_KEY;
+import static org.sonar.api.ce.measure.Component.Type.MODULE;
+import static org.sonar.api.ce.measure.Component.Type.PROJECT;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,7 +46,8 @@ public class LicenseCheckMeasureComputer implements MeasureComputer
     {
         if (settings.getBoolean(ACTIVATION_KEY))
         {
-            if (context.getComponent().getType() == Component.Type.PROJECT)
+            Component.Type type = context.getComponent().getType();
+            if (type == MODULE || type == PROJECT)
             {
                 combineDependencies(context);
 
@@ -57,42 +60,55 @@ public class LicenseCheckMeasureComputer implements MeasureComputer
         }
     }
 
-    private void combineLicenses(MeasureComputerContext context)
+    private static void combineLicenses(MeasureComputerContext context)
     {
         LOGGER.debug("Combining licenses on {}", context.getComponent().getKey());
         List<License> licensesWithChildren = new ArrayList<>();
         Measure measure = context.getMeasure(INPUTLICENSE.getKey());
+        addLicenseMeasure(licensesWithChildren, measure);
+        for (Measure childMeasure : context.getChildrenMeasures(INPUTLICENSE.getKey()))
+        {
+            addLicenseMeasure(licensesWithChildren, childMeasure);
+        }
+        for (Measure childMeasure : context.getChildrenMeasures(LICENSE.getKey()))
+        {
+            addLicenseMeasure(licensesWithChildren, childMeasure);
+        }
+        context.addMeasure(LICENSE.getKey(), License.createString(licensesWithChildren));
+        LOGGER.debug("Stored licenses {}", licensesWithChildren);
+    }
+
+    private static void addLicenseMeasure(final List<License> licensesWithChildren, final Measure measure)
+    {
         if (measure != null && StringUtils.isNotBlank(measure.getStringValue()))
         {
             licensesWithChildren.addAll(License.fromString(measure.getStringValue()));
         }
-        for (Measure childMeasure : context.getChildrenMeasures(INPUTLICENSE.getKey()))
-        {
-            if (childMeasure != null && StringUtils.isNotBlank(childMeasure.getStringValue()))
-            {
-                licensesWithChildren.addAll(License.fromString(measure.getStringValue()));
-            }
-        }
-        context.addMeasure(LICENSE.getKey(), License.createString(licensesWithChildren));
-        LOGGER.debug("Stored {}", context.getMeasure(LICENSE.getKey()));
     }
 
-    private void combineDependencies(MeasureComputerContext context)
+    private static void combineDependencies(MeasureComputerContext context)
     {
         LOGGER.debug("Combining dependencies on {}", context.getComponent().getKey());
         Measure measure = context.getMeasure(INPUTDEPENDENCY.getKey());
         List<Dependency> dependencyWithChildren = new ArrayList<>();
+        addDependencyMeasure(dependencyWithChildren, measure);
+        for (Measure childMeasure : context.getChildrenMeasures(INPUTDEPENDENCY.getKey()))
+        {
+            addDependencyMeasure(dependencyWithChildren, childMeasure);
+        }
+        for (Measure childMeasure : context.getChildrenMeasures(DEPENDENCY.getKey()))
+        {
+            addDependencyMeasure(dependencyWithChildren, childMeasure);
+        }
+        context.addMeasure(DEPENDENCY.getKey(), Dependency.createString(dependencyWithChildren));
+        LOGGER.debug("Stored dependencies {}", dependencyWithChildren);
+    }
+
+    private static void addDependencyMeasure(final List<Dependency> dependencyWithChildren, final Measure measure)
+    {
         if (measure != null && StringUtils.isNotBlank(measure.getStringValue()))
         {
             dependencyWithChildren.addAll(Dependency.fromString(measure.getStringValue()));
         }
-        for (Measure childMeasure : context.getChildrenMeasures(INPUTDEPENDENCY.getKey()))
-        {
-            if (childMeasure != null && StringUtils.isNotBlank(childMeasure.getStringValue()))
-            {
-                dependencyWithChildren.addAll(Dependency.fromString(measure.getStringValue()));
-            }
-        }
-        context.addMeasure(DEPENDENCY.getKey(), Dependency.createString(dependencyWithChildren));
     }
 }
