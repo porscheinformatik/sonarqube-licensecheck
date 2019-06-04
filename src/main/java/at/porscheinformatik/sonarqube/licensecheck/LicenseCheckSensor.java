cddcb7c89f3ca9c0a6fe0ab34805097fc7a4524d
Christian Köberl <derkoe@users.noreply.github.com>
2019-06-04 09:30:45 +0200

    SoarQube 7.7 support, closes #44 (#46)
     - SQ 7.7 does not store measures on modules anymore. A workaround
       is implemented that stores the module's data in a static variable.

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckSensor.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckSensor.java
@@ -1,7 +1,11 @@
 package at.porscheinformatik.sonarqube.licensecheck;
 
+import static java.util.Collections.newSetFromMap;
+
+import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +33,10 @@ public class LicenseCheckSensor implements Sensor
     private final ValidateLicenses validateLicenses;
     private final Scanner[] scanners;
 
+    private final static Set<License> AGGREGATED_LICENSES = newSetFromMap(new ConcurrentHashMap<License, Boolean>());
+    private final static Set<Dependency> AGGREGATED_DEPENDENCIES =
+        newSetFromMap(new ConcurrentHashMap<Dependency, Boolean>());
+
     public LicenseCheckSensor(FileSystem fs, Settings settings, ValidateLicenses validateLicenses,
         MavenLicenseService mavenLicenseService, MavenDependencyService mavenDependencyService)
     {
@@ -42,11 +50,13 @@ public class LicenseCheckSensor implements Sensor
 
     private static void saveDependencies(SensorContext sensorContext, Set<Dependency> dependencies)
     {
+        LOGGER.debug("Saving depenencies for module {}: {}", sensorContext.module(), dependencies);
+
         if (!dependencies.isEmpty())
         {
             sensorContext
-                .newMeasure()
-                .forMetric(LicenseCheckMetrics.INPUTDEPENDENCY)
+                .<String>newMeasure()
+                .forMetric(LicenseCheckMetrics.DEPENDENCY)
                 .withValue(Dependency.createString(dependencies))
                 .on(sensorContext.module())
                 .save();
@@ -55,11 +65,13 @@ public class LicenseCheckSensor implements Sensor
 
     private static void saveLicenses(SensorContext sensorContext, Set<License> licenses)
     {
+        LOGGER.debug("Saving licenses for module {}: {}", sensorContext.module(), licenses);
+
         if (!licenses.isEmpty())
         {
             sensorContext
-                .newMeasure()
-                .forMetric(LicenseCheckMetrics.INPUTLICENSE)
+                .<String>newMeasure()
+                .forMetric(LicenseCheckMetrics.LICENSE)
                 .withValue(License.createString(licenses))
                 .on(sensorContext.module())
                 .save();
@@ -76,25 +88,35 @@ public class LicenseCheckSensor implements Sensor
     @Override
     public void execute(SensorContext context)
     {
-        if (settings.getBoolean(LicenseCheckPropertyKeys.ACTIVATION_KEY))
+        if (!settings.getBoolean(LicenseCheckPropertyKeys.ACTIVATION_KEY))
         {
-            Set<Dependency> dependencies = new TreeSet<>();
+            LOGGER.info("Scanner is set to inactive. No scan possible.");
+            return;
+        }
 
-            for (Scanner scanner : scanners)
-            {
-                dependencies.addAll(scanner.scan(fs.baseDir()));
-            }
+        Set<Dependency> dependencies = new TreeSet<>();
 
-            ProjectDefinition project = LicenseCheckPlugin.getRootProject(((DefaultInputModule) context.module()).definition());
-            Set<Dependency> validatedDependencies = validateLicenses.validateLicenses(dependencies, context);
-            Set<License> usedLicenses = validateLicenses.getUsedLicenses(validatedDependencies, project);
+        for (Scanner scanner : scanners)
+        {
+            dependencies.addAll(scanner.scan(fs.baseDir()));
+        }
 
-            saveDependencies(context, validatedDependencies);
-            saveLicenses(context, usedLicenses);
+        ProjectDefinition project = LicenseCheckPlugin.getRootProject(((DefaultInputModule) context.module()).definition());
+        Set<Dependency> validatedDependencies = validateLicenses.validateLicenses(dependencies, context);
+        Set<License> usedLicenses = validateLicenses.getUsedLicenses(validatedDependencies, project);
+
+        AGGREGATED_LICENSES.addAll(usedLicenses);
+        AGGREGATED_DEPENDENCIES.addAll(validatedDependencies);
+
+        if (project.getParent() == null)
+        {
+            saveDependencies(context, AGGREGATED_DEPENDENCIES);
+            saveLicenses(context, AGGREGATED_LICENSES);
         }
         else
         {
-            LOGGER.info("Scanner is set to inactive. No scan possible.");
+            saveDependencies(context, validatedDependencies);
+            saveLicenses(context, usedLicenses);
         }
     }
 }
