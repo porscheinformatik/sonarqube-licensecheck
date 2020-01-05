a4260871e28b155467e32084febc23a1b6430f5f
Janpopan <janpopan@gmx.net>
2020-01-05 19:12:04 +0100

    add sortable table feature

--- src/main/web/dashboard/licenses.vue
+++ src/main/web/dashboard/licenses.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="boxed-group boxed-group-inner">
     <h3>Licenses</h3>
-    <p>This is a list of all licenses used in any dependencies listed above.</p>
     <table class="data zebra">
+      <caption>This is a list of all licenses used in any dependencies listed above.</caption>
       <thead>
         <tr>
-          <th>Identifier</th>
-          <th>Name</th>
-          <th>Allowed</th>
+          <th @click="sort('identifier')" scope="col">Identifier</th>
+          <th @click="sort('name')" scope="col">Name</th>
+          <th @click="sort('status')" scope="col">Allowed</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="license in licenses">
+        <tr v-for="(license) in sortedLicenses" v-bind:key="license">
           <td>{{license.identifier}}</td>
           <td>{{license.name}}</td>
           <td>
@@ -22,12 +22,44 @@
       </tbody>
     </table>
   </div>
+
+
 </template>
 
 <script>
   export default {
-    props: ['licenses']
+props: ["licenses"],
+  data() {
+    return {
+      license: {
+        identifier: "",
+        name: "",
+        status: ""
+      },
+      sortBy: "identifier",
+      sortDirection: "asc"
+    };
+  },
+  computed: {
+    sortedLicenses() {
+      return this.licenses.sort((a, b) => {
+        let modifier = 1;
+        if (this.sortDirection === "desc") modifier = -1;
+        if (a[this.sortBy] < b[this.sortBy]) return -1 * modifier;
+        if (a[this.sortBy] > b[this.sortBy]) return 1 * modifier;
+        return 0;
+      });
+    }
+  },
+  methods: {
+    sort(param) {
+      if (param === this.sortBy) {
+        this.sortDirection = this.sortDirection === "asc" ? "desc" : "asc";
+      }
+      this.sortBy = param;
+    }
   }
+};
 </script>
 
 <style>
