beb7c56bb61fd69eea3d4d0521012c0d95c9cca5
Jakob Berlin <j.berlin@ewerk.com>
2020-09-30 14:14:20 +0200

    Address some of the (partly weird) codeclimate issues
    (cherry picked from commit c30d2c8a4e0692e4043a819ca4b14f4e2641053e)

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/webservice/configuration/MavenLicenseConfiguration.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/webservice/configuration/MavenLicenseConfiguration.java
@@ -1,6 +1,6 @@
 package at.porscheinformatik.sonarqube.licensecheck.webservice.configuration;
 
-public final class MavenLicenseConfiguration
+public final class MavenLicenseConfiguration extends LicenseCheckConfiguration
 {
     private MavenLicenseConfiguration()
     {
@@ -9,14 +9,8 @@ public final class MavenLicenseConfiguration
 
     public static final String PARAM_REGEX = "regex";
     public static final String PARAM_OLD_REGEX = "old_regex";
-    public static final String PARAM_LICENSE = "license";
     public static final String JSON_ARRAY_NAME = "mavenLicenses";
     public static final String CONTROLLER = "api/licensecheck/maven-licenses";
-
-    public static final String SHOW_ACTION = "show";
-    public static final String DELETE_ACTION = "delete";
-    public static final String ADD_ACTION = "add";
-    public static final String EDIT_ACTION = "edit";
 
     public static final String SHOW_ACTION_DESCRIPTION = "Show Maven Licenses";
     public static final String DELETE_ACTION_DESCRIPTION = "Delete Maven License";
@@ -24,7 +18,6 @@ public final class MavenLicenseConfiguration
     public static final String EDIT_ACTION_DESCRIPTION = "Edit Maven License";
 
     public static final String PROPERTY_REGEX = "regex";
-    public static final String PROPERTY_LICENSE = "license";
 
     public static final String ERROR_EDIT_ALREADY_EXISTS = "Edit Maven License aborted. Maven License already exists: ";
     public static final String INFO_EDIT_SUCCESS = "Maven License edited: ";
