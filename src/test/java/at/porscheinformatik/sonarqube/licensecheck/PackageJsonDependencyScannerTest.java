ed2cc3b584a006677a11600f4777ac91c40b8b0c
Jakob Berlin <j.berlin@ewerk.com>
2020-10-08 12:12:22 +0200

    Fix tests

--- src/test/java/at/porscheinformatik/sonarqube/licensecheck/PackageJsonDependencyScannerTest.java
+++ src/test/java/at/porscheinformatik/sonarqube/licensecheck/PackageJsonDependencyScannerTest.java
@@ -1,17 +1,17 @@
 package at.porscheinformatik.sonarqube.licensecheck;
 
-import java.io.File;
-import java.util.Set;
-
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import at.porscheinformatik.sonarqube.licensecheck.interfaces.Scanner;
+import at.porscheinformatik.sonarqube.licensecheck.npm.PackageJsonDependency;
 import at.porscheinformatik.sonarqube.licensecheck.npm.PackageJsonDependencyScanner;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class PackageJsonDependencyScannerTest
 {
@@ -24,10 +24,13 @@ public class PackageJsonDependencyScannerTest
 
         Set<Dependency> dependencies = scanner.scan(folder);
 
-        assertThat(dependencies, hasSize(2));
-        assertThat(dependencies, containsInAnyOrder(
-            new Dependency("angular", "1.5.0", "MIT"),
-            new Dependency("arangojs", "5.6.0", "Apache-2.0")));
+        assertEquals(2, dependencies.size());
+        Assert.assertTrue(dependencies.containsAll(new ArrayList<PackageJsonDependency>() {
+            {
+                add(new PackageJsonDependency("angular", "1.5.0", "MIT"));
+                add(new PackageJsonDependency("arangojs", "5.6.0", "Apache-2.0"));
+            }
+        }));
     }
 
     @Test
@@ -37,12 +40,15 @@ public class PackageJsonDependencyScannerTest
 
         Set<Dependency> dependencies = scanner.scan(folder);
 
-        assertThat(dependencies, hasSize(4));
-        assertThat(dependencies, containsInAnyOrder(
-            new Dependency("angular", "1.5.0", "MIT"),
-            new Dependency("arangojs", "5.6.0", "Apache-2.0"),
-            new Dependency("linkedlist", "1.0.1", "LGPLv3"),
-            new Dependency("retry", "0.10.1", "MIT")));
+        assertEquals(4, dependencies.size());
+        assertTrue(dependencies.containsAll(new ArrayList<PackageJsonDependency>() {
+            {
+                add(new PackageJsonDependency("angular", "1.5.0", "MIT"));
+                add(new PackageJsonDependency("arangojs", "5.6.0", "Apache-2.0"));
+                add(new PackageJsonDependency("linkedlist", "1.0.1", "LGPLv3"));
+                add(new PackageJsonDependency("retry", "0.10.1", "MIT"));
+            }
+        }));
     }
 
     @Test
@@ -52,7 +58,7 @@ public class PackageJsonDependencyScannerTest
 
         Set<Dependency> dependencies = scanner.scan(new File("src"));
 
-        assertThat(dependencies, hasSize(0));
+        assertEquals(0, dependencies.size());
     }
 
     @Test
@@ -62,7 +68,7 @@ public class PackageJsonDependencyScannerTest
 
         Set<Dependency> dependencies = scanner.scan(new File(folder, "node_modules/arangojs"));
 
-        assertThat(dependencies, hasSize(0));
+        assertEquals(0, dependencies.size());
     }
 
     @Test
@@ -74,7 +80,7 @@ public class PackageJsonDependencyScannerTest
 
         assertEquals(1, dependencies.size());
 
-        final Dependency expectedDependency = new Dependency("dynamic-dedupe", "0.3.0", "MIT");
+        final Dependency expectedDependency = new PackageJsonDependency("dynamic-dedupe", "0.3.0", "MIT");
         assertEquals(expectedDependency, dependencies.toArray()[0]);
     }
 
@@ -87,7 +93,7 @@ public class PackageJsonDependencyScannerTest
 
         assertEquals(1, dependencies.size());
 
-        final Dependency expectedDependency = new Dependency("some-module", "1.7.1", "(MIT OR LGPLv3)");
+        final Dependency expectedDependency = new PackageJsonDependency("some-module", "1.7.1", "(MIT OR LGPLv3)");
         assertEquals(expectedDependency, dependencies.toArray()[0]);
     }
 }
