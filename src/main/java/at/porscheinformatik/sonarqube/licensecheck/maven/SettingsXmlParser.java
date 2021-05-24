7393d085694bb07528d8d4855c0c1621dddb4321
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-05-24 16:04:26 +0200

    fix: disable external entities (prevent XXE)

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/maven/SettingsXmlParser.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/maven/SettingsXmlParser.java
@@ -2,26 +2,23 @@ package at.porscheinformatik.sonarqube.licensecheck.maven;
 
 import java.io.File;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
-
 class SettingsXmlParser extends SettingsXmlHandler
 {
-
     private static final Logger LOGGER = Loggers.get(SettingsXmlParser.class);
 
     private SettingsXmlParser()
     {
-
     }
 
     public static Setting parseXmlFile(File filePath)
     {
-
         SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
         SettingsXmlHandler settingsXmlHandler = new SettingsXmlHandler();
         SAXParser saxParser;
@@ -31,6 +28,8 @@ class SettingsXmlParser extends SettingsXmlHandler
             try
             {
                 saxParser = saxParserFactory.newSAXParser();
+                saxParser.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+                saxParser.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
                 saxParser.parse(filePath, settingsXmlHandler);
             }
             catch (Exception e)
@@ -44,6 +43,5 @@ class SettingsXmlParser extends SettingsXmlHandler
         }
 
         return settingsXmlHandler.getSetting();
-
     }
 }
