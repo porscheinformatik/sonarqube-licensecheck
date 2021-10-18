2275cc0886497883ad1610627cf8bd46ddac03a5
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-10-18 21:52:41 +0200

    chore: restore default sonar.sources, verify in tests

--- src/test/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckPluginTest.java
+++ src/test/java/at/porscheinformatik/sonarqube/licensecheck/LicenseCheckPluginTest.java
@@ -1,6 +1,8 @@
 package at.porscheinformatik.sonarqube.licensecheck;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.sonar.api.Plugin;
@@ -10,7 +12,10 @@ public class LicenseCheckPluginTest
     @Test
     public void define()
     {
+        Plugin.Context context = mock(Plugin.Context.class);
         LicenseCheckPlugin plugin = new LicenseCheckPlugin();
-        plugin.define(mock(Plugin.Context.class));
+        plugin.define(context);
+
+        verify(context).addExtensions(any());
     }
 }
