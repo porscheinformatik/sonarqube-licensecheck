b99088f440db4edb525ca9d09c1054ba162ae497
Christian Köberl <christian.koeberl@porscheinformatik.com>
2023-10-09 16:46:36 +0200

    Fix small code issues

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/npm/PackageJsonDependencyScanner.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/npm/PackageJsonDependencyScanner.java
@@ -3,7 +3,6 @@ package at.porscheinformatik.sonarqube.licensecheck.npm;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
