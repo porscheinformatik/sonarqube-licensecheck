6fb70114a9b3a720b4a92f06e6d90c836e7aefbc
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-06-23 16:35:47 +0200

    fix: also use maven.repo.local inside MAVEN_OPTS

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/maven/DirectoryFinder.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/maven/DirectoryFinder.java
@@ -2,8 +2,13 @@ package at.porscheinformatik.sonarqube.licensecheck.maven;
 
 import at.porscheinformatik.sonarqube.licensecheck.Dependency;
 import java.io.File;
+import org.apache.maven.shared.utils.cli.CommandLineUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class DirectoryFinder {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DirectoryFinder.class);
 
     private DirectoryFinder() {}
 
@@ -30,6 +35,21 @@ class DirectoryFinder {
     public static File getMavenRepsitoryDir(String userSettings, String globalSettings) {
         if (System.getProperty("maven.repo.local") != null) {
             return new File(System.getProperty("maven.repo.local"));
+        }
+        String mavenOpts = System.getenv("MAVEN_OPTS");
+        if (mavenOpts != null) {
+            try {
+                String[] opts = CommandLineUtils.translateCommandline(mavenOpts);
+                for (String opt : opts) {
+                    if (opt.startsWith("-Dmaven.repo.local=")) {
+                        String repoPath = opt.substring("-Dmaven.repo.local=".length());
+                        return new File(repoPath);
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.warn("Could not parse MAVEN_OPTS: " + mavenOpts, e);
+                // ignore
+            }
         }
 
         File mavenConfFile = new File(System.getProperty("user.home"), ".m2/settings.xml");
